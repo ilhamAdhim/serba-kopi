130172a505446fd58daf75a94911f708dc98f66f
ilhamadhim <ilhamm179@gmail.com>
2020-07-10 16:53:09 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Activate sidebar nav
+    let elems = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(elems);
+    loadNav();
+
+    function loadNav() {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status != 200) return;
+
+                // Muat daftar tautan menu
+                document.querySelectorAll(".topnav, .sidenav").forEach(function (elm) {
+                    elm.innerHTML = xhttp.responseText;
+                });
+
+                // Daftarkan event listener untuk setiap tautan menu
+                document.querySelectorAll(".sidenav a, .topnav a").forEach(elm => {
+                    elm.addEventListener("click", function (event) {
+                        // Tutup sidenav
+                        let sidenav = document.querySelector(".sidenav");
+                        M.Sidenav.getInstance(sidenav).close();
+
+                        // Muat konten halaman yang dipanggil
+                        page = event.target.getAttribute("href").substr(1);
+                        console.log(page)
+                        loadPage(page);
+                    });
+                });
+            }
+        };
+        xhttp.open("GET", "nav.html", true);
+        xhttp.send();
+    }
+
+
+    // Load page content
+    let page = window.location.hash.substr(1);
+    if (page == "") page = "home";
+    loadPage(page);
+
+    function loadPage(page) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                let content = document.querySelector("#body-content");
+                if (this.status == 200) {
+                    content.innerHTML = xhttp.responseText;
+                } else if (this.status == 404) {
+                    // Jika user online, tapi file html tidak ada di server
+                    content.innerHTML = `
+                    <div class="container" style="height:80vh"> 
+                        <div class="center"> 
+                            <h1> 404 Not Found </h1>
+                            Halaman tidak ditemukan
+                        </div>
+                    </div>
+                    `;
+                } else {
+                    // Jika user offline
+                    content.innerHTML = `
+                    <div class="container" style="height:80vh"> 
+                        <div class="center">
+                        <img src="../images/offline.png"> <br>
+                            Anda sedang offline
+                        </div>
+                    </div>`;
+                }
+            }
+        };
+        xhttp.open("GET", "pages/" + page + ".html", true);
+        xhttp.send();
+    }
+});
---
+++ service-worker.js
@@ -0,0 +1,75 @@
+// Register Cache to service - worker
+const CACHE_NAME = 'serba-kopi-v2';
+let urlsToCache = [
+    '/',
+    '/iconkopi.png',
+    '/nav.html',
+    '/index.html',
+    '/pages/home.html',
+    '/pages/jenisKopi.html',
+    '/pages/kreasiKopi.html',
+    '/css/materialize.min.css',
+    '/css/style.css',
+    '/js/materialize.min.js',
+    '/js/script.js',
+    '/images/affogato.jpg',
+    '/images/americano.jpeg',
+    '/images/brain.png',
+    '/images/bullseye.png',
+    '/images/capucino.jpg',
+    '/images/choco-coffee.jpg',
+    '/images/coffee-jumbotron.jpeg',
+    '/images/coffee-latte.jpg',
+    '/images/coffee-mix.jpeg',
+    '/images/cold-brew.jpg',
+    '/images/espresso-chill.jpg',
+    '/images/espresso.jpg',
+    '/images/homemade-coffee.jpg',
+    '/images/idea.png',
+    '/images/latte.jpg',
+    '/images/moccachino.jpg',
+    './images/offline.png'
+];
+
+// Using assets from browser cache 
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+})
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches
+            .match(event.request, { cacheName: CACHE_NAME })
+            .then(response => {
+                if (response) {
+                    console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                    return response;
+                }
+
+                console.log(
+                    "ServiceWorker: Memuat aset dari server: ",
+                    event.request.url
+                );
+                return fetch(event.request);
+            })
+    );
+});
+
+// Delete Old Cache in case of updating the web layout or content
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
